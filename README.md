fda64ca774ac637167104fca40b7d65864ff58b5
deanone <athanasios.salamanis@gmail.com>
2021-11-03 14:49:52 +0200

    Added the linear search algorithm.

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
-This repo contains native C implementations of popular search algorithms (apart from linear search), namely:
+This repo contains native C implementations of popular search algorithms, namely:
 
-1. Binary search (https://en.wikipedia.org/wiki/Binary_search_algorithm)
-2. Jump search (https://en.wikipedia.org/wiki/Jump_search)
-3. Interpolation search (https://en.wikipedia.org/wiki/Interpolation_search)
-4. Exponential search (https://en.wikipedia.org/wiki/Exponential_search)
-5. Fibonacci search (https://en.wikipedia.org/wiki/Fibonacci_search_technique)
+1. Linear search (https://en.wikipedia.org/wiki/Linear_search)
+2. Binary search (https://en.wikipedia.org/wiki/Binary_search_algorithm)
+3. Jump search (https://en.wikipedia.org/wiki/Jump_search)
+4. Interpolation search (https://en.wikipedia.org/wiki/Interpolation_search)
+5. Exponential search (https://en.wikipedia.org/wiki/Exponential_search)
+6. Fibonacci search (https://en.wikipedia.org/wiki/Fibonacci_search_technique)
 
 Compile:
 
@@ -12,4 +13,4 @@ Compile:
 
 Run:
 
-./search.out [number of values of array in which key is searched] [value to search for]
+./search.out [id of search algorithm] [number of values of array in which key is searched] [value to search for]
--- search.c
+++ search.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <time.h>
+#include <stdbool.h>
+
+int linearSearch(int* arr, int n, int val)
+{
+	bool found = false;
+	int i = 0;
+	for (i = 0; i < n; ++i)
+	{
+		if (arr[i] == val)
+		{
+			found = true;
+			break;
+		}
+	}
+
+	if (found)
+	{
+		return i;
+	}
+	else
+	{
+		return - 1;
+	}
+}
 
 int binarySearch(int* arr, int n, int val)
 {
@@ -29,22 +54,44 @@ int binarySearch(int* arr, int n, int val)
 
 int main(int argc, char const *argv[])
 {
-	int n = atoi(argv[1]);
-	int val = atoi(argv[2]);
+	int algorithmID = atoi(argv[1]);
+	int n = atoi(argv[2]);
+	int val = atoi(argv[3]);
 	int* arr = malloc(n * sizeof(int));
 	for (int i = 0; i < n; ++i)
 	{
 		arr[i] = i;
 	}
+	double elapsedTime = -1.0;
+	int keyIndex = -1;
 
-	int keyIndex = binarySearch(arr, n, val);
+	switch (algorithmID)
+	{
+		case 1:
+		{
+			clock_t start = clock();
+			keyIndex = linearSearch(arr, n, val);
+			clock_t end = clock();
+			elapsedTime = (double)(end - start) / CLOCKS_PER_SEC;
+			break;
+		}
+		case 2:
+		{
+			clock_t start = clock();
+			keyIndex = binarySearch(arr, n, val);
+			clock_t end = clock();
+			elapsedTime = (double)(end - start) / CLOCKS_PER_SEC;
+			break;
+		}
+	}
+
 	if (keyIndex == -1)
 	{
 		printf("Key not found.\n");
 	}
 	else
 	{
-		printf("Key found in index %d.\n", keyIndex);	
+		printf("Key found in index %d in %f seconds.\n", keyIndex, elapsedTime);	
 	}
 	
 	return 0;
